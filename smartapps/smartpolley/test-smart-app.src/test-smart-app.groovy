21c625f9f03cb9954a384188c3495029a7f8b9c0
Will Polley <willpolley@gmail.com>
2016-10-29 21:53:09 -0400

    rr

---
+++ smartapps/smartpolley/test-smart-app.src/test-smart-app.groovy
@@ -0,0 +1,42 @@
+/**
+ *  Test Smart App
+ *
+ *  Copyright 2016 Will Polley
+ *
+ */
+definition(
+    name: "Test Smart App",
+    namespace: "smartpolley",
+    author: "Will Polley",
+    description: "This is a test for structure.",
+    category: "Convenience",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    oauth: true)
+
+
+preferences {
+	section("Title") {
+		// TODO: put inputs here
+	}
+}
+
+def installed() {
+	log.debug "Installed with settings: ${settings}"
+
+	initialize()
+}
+
+def updated() {
+	log.debug "Updated with settings: ${settings}"
+
+	unsubscribe()
+	initialize()
+}
+
+def initialize() {
+	// TODO: subscribe to attributes, devices, locations, etc.
+}
+
+// TODO: implement event handlers
